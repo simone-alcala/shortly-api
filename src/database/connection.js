8c92d69236b95faa36bd65b45d5f6e5bdb4ac4f1
simone-alcala <simone.alcala@gmail.com>
2022-06-09 19:03:32 -0300

    FIX: project changes

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,38 +1,3 @@
-/*import dotenv from 'dotenv';
-import pg from 'pg';
-
-const { Pool } = pg;
-
-dotenv.config();
-
-const db = new Pool({
-  connectionString: process.env.DATABASE_URL
-});
-
-if (process.env.MODE === 'PROD') 
-  db.ssl= { rejectUnauthorized: false };
-
-
-export default db;*/
-/*
-import dotenv from 'dotenv';
-import pkg from 'pg';
-const { Pool } = pkg;
-
-dotenv.config();
-
-const databaseConfig = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-}
-
-const db = new Pool(databaseConfig);
-
-export default db;
-*/
-
 import dotenv from 'dotenv';
 import pkg from 'pg';
 const { Pool } = pkg;
--- src/routers/usersRouter.js
+++ src/routers/usersRouter.js
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import { validateId } from './../middlewares/validateUser.js';
-
+import validateSession from './../middlewares/validateSession.js';
 import { getUserById , getUsersRanking} from './../controllers/usersController.js';
 
 const usersRouter = Router();
 
-usersRouter.get('/users/ranking', getUsersRanking );
-usersRouter.get('/users/:id', validateId, getUserById );
+usersRouter.get('/ranking', getUsersRanking );
+usersRouter.get('/users/:id', validateSession, validateId, getUserById );
 
 export default usersRouter;
